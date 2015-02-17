af86ccdba5010e6767b61db903dcd2811e441931
Craig <pureferret@gmail.com>
2015-02-17 16:27:10 +0000

    Djangularised characters app

---
+++ characters/app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('characters2.controllers', []).
+    controller('MyCtrl1', [function() {
+
+    }])
+    .controller('MyCtrl2', [function() {
+
+    }]);
